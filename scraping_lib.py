4626a3be0ca734419b6e57692360bb9203cad13c
Akkiwadhwa <akkiwadhwa1995@gmail.com>
2025-04-09 23:00:47 +0530

    `Revert chrome_options arguments`

--- scraping_lib.py
+++ scraping_lib.py
@@ -15,8 +15,8 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 def initialize_driver():
     chrome_options = Options()
-    #chrome_options.add_argument("--headless")
-    #chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("--disable-dev-shm-usage")
